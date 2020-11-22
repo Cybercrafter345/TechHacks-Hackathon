8102e475b97d61d2cbf205e8cb1dc50778dc4744
SantiVira <67117641+SantiVira@users.noreply.github.com>
2020-11-21 23:39:49 -0500

    Lots of stuff

--- public/Quiz Stuff/home.js
+++ public/Quiz Stuff/home.js
@@ -1,7 +1,7 @@
 //number of questions per quiz
-let numQuezs=2
+let numQuezs=8
 var score=0
-let nextQ=false
+let Qcounter=0
 
 
 //function called one the start button is pressed
@@ -12,8 +12,9 @@ const quiz = async () => {
 
             //Turn the json string into an HTML div
             var questions=generate()
+            ez=questions[0], med=questions[1], hard=questions[2]
             //Randomly choose questions from that list
-            chosen = getRandom(questions, numQuezs)
+            chosen = getRandom(med, Math.floor(numQuezs/2))
             //display the first question
             display()
         
@@ -21,12 +22,31 @@ const quiz = async () => {
 
 //function to display a question
 function display(){
+    Qcounter++
+    console.log(Qcounter)
+    console.log(chosen)
     if(chosen.length==0){
+        if(Qcounter-1==numQuezs){
         EndScreen()
+        }
+        else{
+            if(score<Math.ceil(numQuezs/2)) {chosen = getRandom(ez, Math.floor(numQuezs/2))}
+            else if(score==Math.ceil(numQuezs/2)){chosen = getRandom(med, Math.floor(numQuezs/2))}
+            else {chosen = getRandom(hard, Math.floor(numQuezs/2))}
+            let q=chosen[0]
+            document.body.appendChild(q)
+            chosen.shift()
+
+            let numlab=document.getElementById('PlaceholderToReplace')
+            numlab.innerText=Qcounter
+        }
     }else{
     let q=chosen[0]
     document.body.appendChild(q)
     chosen.shift()
+
+    let numlab=document.getElementById('PlaceholderToReplace')
+    numlab.innerText=Qcounter
     }
 }
 //function to choose randomly from an array
@@ -45,10 +65,11 @@ function getRandom(arr, n) {
 }
 //function to gather all the html divs
 function generate(){
-    let questions=[]
+    let questions=[[],[],[]]
     for(var i in QA){
-        let div=quez(QA[i])
-        questions.push(div)
+        let res=quez(QA[i])
+        let div=res[0]
+        questions[res[1]].push(div)
     }
     
     return questions
@@ -109,7 +130,7 @@ function quez(obj){
             let d=document.getElementById(allname)
             let b=document.createElement('BUTTON')
             b.className='start'
-            var t = document.createTextNode("Next -->");
+            var t = document.createTextNode("Next");
             b.appendChild(t);
             function fadeout(){
                 let d=document.getElementById('qpage')
@@ -142,9 +163,21 @@ function quez(obj){
             };
             return a
         }
+        function spant(sp, txt){
+            let h = document.createElement("p")
+            let t = document.createTextNode(txt);
+            let s= document.createElement("SPAN")
+            let spt = document.createTextNode(sp);
+            s.appendChild(spt);   
+            s.id='PlaceholderToReplace'
+            h.append(s)
+            h.append(t)
+            
+            return h
+        }
 
         //add the text to the question div
-        question.appendChild(text(obj.q))
+        question.appendChild(spant('', ' '+obj.q))
         //shuffle the answers and give them correct or incorrect status
         let as=shuffle([
             correct(obj.a),
@@ -156,7 +189,7 @@ function quez(obj){
         for(var i in as){answers.appendChild(as[i])}
 
         //return the overall container div
-        return All
+        return [All, obj.d-1]
     }
 
 //function to make the final screen
@@ -211,6 +244,8 @@ var homescreen=`
 <br>
     <button onclick='quiz()' id='start' class='start'>Start</button>`
 function GoBack(){
+    score=0
+    Qcounter=0
     let d=document.body
     while (d.firstChild) {
         d.removeChild(d.firstChild);
@@ -230,17 +265,243 @@ function GoBack(){
 //json containing all of the questions
 let QA=[
     {
-        "q": "Why is it called Covid - NINETEEN ?",
-        "a": "It is because it was discovered in 2019",
-        "a1": "It is because there are 19 known strands",
-        "a2": "It is because there are 19 stems on the molecule",
-        "a3": "It is because there have been 19 previous coronavirus'"
+        "q": "What Part of the body does the Coronavirus Affect",
+        "a": "The Lungs/Airways",
+        "a1": "The Heart/Blood Vessels",
+        "a2": "The Lower Body",
+        "a3": "The Brain and Nervous System",
+        "d": 1
     },
     {
-        "q": "Why is it called Covid - NINeeeeeeeeeeeeeeeeeeeETEEN ?",
-        "a": "It is because it was discovered in 2019",
-        "a1": "It is because there are 19 known strands",
-        "a2": "It is because there are 19 stems on the molecule",
-        "a3": "It is because there have been 19 previous coronavirus'"
+        "q": "What is not a symptom of the Coronavirus",
+        "a": "Loss of appetite",
+        "a1": "Fever",
+        "a2": "Loss of taste/smell",
+        "a3": "Tiredness",
+        "d": 2
+    },
+    {
+        "q": "Where was the Coronavirus first discovered",
+        "a": "Wuhan, China",
+        "a1": "New York, USA",
+        "a2": "Volgograd, Russia",
+        "a3": "Wales, Britain",
+        "d": 3
+    },
+    {
+        "q": "People with pre-existing illnesses are more vulnerable to Coronavirus",
+        "a": "True, pre-existing conditions weaken your immune system and leave you compromised",
+        "a1": "True because pre-existing conditions can cause coronavirus",
+        "a2": "False, your immune system can fight both pre-existing conditions and covid",
+        "a3": "False, pre-existing conditions prevent you from getting coronavirus",
+        "d": 1
+    },
+    {
+        "q": "How far do you have to be at a minimum from each other to be safe from Coronavirus",
+        "a": "6 Feet",
+        "a1": "2 Feet",
+        "a2": "15 Feet",
+        "a3": "11 Feet",
+        "d": 2
+    },
+    {
+        "q": "Where does Coronavirus spread easiest",
+        "a": "In an urban setting",
+        "a1": "In a rural setting",
+        "a2": "In a suburban setting",
+        "a3": "In water",
+        "d": 3
+    },
+    {
+        "q": "Wearing a mask help reduce the spread of Coronavirus",
+        "a": "TRUE because our nose and mouth remains covered when moving and talking",
+        "a1": "TRUE because wearing the mask automatically gets rid of all coronavirus germs",
+        "a2": "FALSE because covering our mouth and nose does not prevent the spread of coronavirus",
+        "a3": "FALSE because masks are just bothersome and do not really help",
+        "d": 1
+    },
+    {
+        "q": "Getting a flu vaccine helps you just in case, you get both the flu and Coronavirus at the same time",
+        "a": "True, the flu vaccine helps you fight the flu, which lets your body focus more on the coronavirus",
+        "a1": "False, the flu vaccine is completely ineffective",
+        "a2": "True, the flu vaccine helps fight both the flu and Coronavirus",
+        "a3": "False, the flu vaccine worsens your symptoms if you have both the coronavirus and the flu",
+        "d": 2
+    },
+    {
+        "q": "What does the 19 in COVID-19 stand for",
+        "a": "2019, the year it was discovered",
+        "a1": "the 19th strain of Coronavirus",
+        "a2": "the 19 countries its affected so far",
+        "a3": "the 19 proteins it has in its shell",
+        "d": 3
+    },
+    {
+        "q": "A vaccine helps you immune system produce antibodies against a virus",
+        "a": "TRUE because it learned about how to protect the body against that disease",
+        "a1": "TRUE because it did not learn about how to protect the body against that disease",
+        "a2": "FALSE because it did not learn about how to protect the body against that disease",
+        "a3": "FALSE because it learned about how to protect the body against that disease",
+        "d": 1
+    },
+    {
+        "q": "The Coronavirus is mutating rapidly",
+        "a": "True because of a lack of safety precautions and the fact that it is airborne",
+        "a1": "False because there is a set speed viruses can spread",
+        "a2": "False because everyone is wearing a mask and staying safe",
+        "a3": "True because it travelled all the way from China",
+        "d": 2
+    },
+    {
+        "q": "What does a phase 3 when testing a vaccine mean",
+        "a": "The Coronavirus vaccine is tested on thousands of people",
+        "a1": "There are three people testing Coronavirus",
+        "a2": "The process of making the vaccine is changed to phase three, making it more effective",
+        "a3": "The vaccine is simulated in a supercomputer",
+        "d": 3
+    },
+    {
+        "q": "Where was the first known death due to COVID-19 in the US?",
+        "a": "California",
+        "a1": "New York",
+        "a2": "Texas",
+        "a3": "Florida",
+        "d": 1
+    },
+    {
+        "q": "Who was the first governor to close schools in response to the pandemic?",
+        "a": "Mike DeWine (Ohio\u2019s governor)",
+        "a1": "Andrew Cuomo (New York's governor)",
+        "a2": "Phil Murphy (New Jersey's Governor)",
+        "a3": "Jared Polis (Colorado's Governor)",
+        "d": 2
+    },
+    {
+        "q": "About what percentage of infected people recover without needing hospital treatment according to the World Health Organization website?",
+        "a": "80%",
+        "a1": "70%",
+        "a2": "50%",
+        "a3": "60%",
+        "d": 3
+    },
+    {
+        "q": "How big is the coronavirus?",
+        "a": "80 billionths of a meter in diameter",
+        "a1": "8 billionths of a meter in diameter",
+        "a2": "800 billionths of a meter in diameter",
+        "a3": "800 millionths of a meter in diameter",
+        "d": 1
+    },
+    {
+        "q": "What does the virus attach itself to when it enters the human body?",
+        "a": "Ace-2 receptors in the lining of the airways",
+        "a1": "Red blood cells",
+        "a2": "Antigens",
+        "a3": "White blood cells",
+        "d": 2
+    },
+    {
+        "q": "How long can the virus survive on plastic and stainless steel surfaces according to studies?\n",
+        "a": "72 hours or more",
+        "a1": "10-20 hours",
+        "a2": "30-50 hours",
+        "a3": "60-70 hours",
+        "d": 3
+    },
+    {
+        "q": "Once infected with the coronavirus, how many days can it take to show symptoms?",
+        "a": "2 to 14 days",
+        "a1": "7-14 days",
+        "a2": "2-12 days",
+        "a3": "7-18 days",
+        "d": 1
+    },
+    {
+        "q": "A high temperature and a continuous cough have been official coronavirus symptoms in the UK since the start of the pandemic. But which of these did the government add to the list of early symptoms that people should look for?",
+        "a": "Losing your sense of smell or taste",
+        "a1": "Diarrhea",
+        "a2": "Stomach Ache",
+        "a3": "Runny Nose",
+        "d": 2
+    },
+    {
+        "q": "The coronavirus is more contagious than the flu.",
+        "a": "TRUE, Because it it airborne",
+        "a1": "TRUE, Because it can't be killed with loud music unlike the flu",
+        "a2": "FALSE, because they are both equally contagius since they are the same thing",
+        "a3": "FALSE, the flu is deadlier and more contagius",
+        "d": 3
+    },
+    {
+        "q": "When was the first case of COVID-19?",
+        "a": "17-Nov-19",
+        "a1": "16-Nov-19",
+        "a2": "15-Nov-19",
+        "a3": "20-Nov-19",
+        "d": 1
+    },
+    {
+        "q": "Antiobiotics can treat coronavirus.",
+        "a": "FALSE - Antibiotics can not treat viruses",
+        "a1": "FALSE - We haven't developed the right Antibiotics yet",
+        "a2": "TRUE - They help lessen symptoms ",
+        "a3": "TRUE - They have always been able to treat any type of virus",
+        "d": 2
+    },
+    {
+        "q": "Which of the following people is COVID-19 more dangerous for?",
+        "a": "Elders and people with underlying health conditions",
+        "a1": "Average Adults",
+        "a2": "Babies",
+        "a3": "Teenagers",
+        "d": 3
+    },
+    {
+        "q": "How long does the novel coronavirus survive outside the body?",
+        "a": "Several hours to days",
+        "a1": "Several minutes",
+        "a2": "Several seconds",
+        "a3": "Several months",
+        "d": 1
+    },
+    {
+        "q": "Covid-19 infects people by injecting its genome into their cells. What type of genetic material is the Covid-19 genome made up of?\n",
+        "a": "RNA",
+        "a1": "DNA",
+        "a2": "Mitchondria",
+        "a3": "Ribosome",
+        "d": 2
+    },
+    {
+        "q": "How should a mask be properly worn?",
+        "a": "Put it over your nose and mouth and secure it under your chin",
+        "a1": "Put it under your nose and mouth and secure it under your chin",
+        "a2": "Put it over your eyes",
+        "a3": "Just cover the mouth and not the nose",
+        "d": 3
+    },
+    {
+        "q": "What is a fomite?",
+        "a": "a contaminated object or surface",
+        "a1": "a contaminated person",
+        "a2": "another name for coronavirus",
+        "a3": "an uncontaminated object or surface",
+        "d": 1
+    },
+    {
+        "q": "Can letters, products and packages be contaminated by the coronavirus (COVID-19) virus?",
+        "a": "Yes because it can carry germs",
+        "a1": "No because it does not carry any outside germs",
+        "a2": "Yes because they do not have masks on them",
+        "a3": "No because they are always clean with no germs",
+        "d": 2
+    },
+    {
+        "q": "On March 1st, there were fewer than a dozen known COVID-19 related deaths in the US. Two months later, how many COVID-19 deaths were reported in the US?",
+        "a": "As of May 1, there were about 60,000 deaths in the US.",
+        "a1": "As of May 1, there were about 600,000 deaths in the US.",
+        "a2": "As of May 1, there were about 6,000 deaths in the US.",
+        "a3": "As of May 1, there were about 600 deaths in the US.",
+        "d": 3
     }
 ]
--- public/Quiz Stuff/json/main.py
+++ public/Quiz Stuff/json/main.py
@@ -5,6 +5,19 @@ data=[]
 
 for rownum in range(len(df.index)):
     r=df.iloc[rownum]
-    data.append({'q':r[0], 'a':r[1], 'a1':r[2], 'a2':r[3], 'a3':r[4]})
+    data.append({'q':r[1], 'a':r[2], 'a1':r[3], 'a2':r[4], 'a3':r[5], 'd':r[0]})
 
-with open('qANDa.json', 'w') as outfile: json.dump(data, outfile, indent=4)
+import numpy as np
+
+class NpEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        elif isinstance(obj, np.floating):
+            return float(obj)
+        elif isinstance(obj, np.ndarray):
+            return obj.tolist()
+        else:
+            return super(NpEncoder, self).default(obj)
+
+with open('qanda.json', 'w') as outfile: json.dump(data, outfile, indent=4, cls=NpEncoder)
