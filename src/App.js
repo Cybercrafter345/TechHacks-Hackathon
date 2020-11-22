299b2b754fcc2dc6289ede1f29ef1f2c5641e967
Cybercrafter345 <49823186+Cybercrafter345@users.noreply.github.com>
2020-11-22 11:52:33 -0500

    test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  var apiGet = 'https://console.echoAR.xyz/query?key=damp-lab-2689&entry=a8dbcf6b-04af-4247-8b80-80368e4796d7';
+  var objsrc = "obj: url("+apiGet+");";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <iframe src="https://console.echoAR.xyz/webxr?key=damp-lab-2689&entry=dea8c460-b7fe-4f8a-93ce-3bebad4c9c39"/>
   );
 }
 
